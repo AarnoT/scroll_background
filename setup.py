0c81034ffc8c45ccb351074358e7ae1ed0ca9d19
AarnoT <AarnoT@users.noreply.github.com>
2017-05-14 14:07:29 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""A basic setup.py.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='scrolling_background',
+    version='0.1.1',
+    author='AarnoT',
+    url='https://github.com/aarnot/scrolling_background',
+    packages=find_packages('src', exclude=['docs', 'tests', 'env']),
+    package_dir={'': 'src'},
+    install_requires=['pygame == 1.9.3'],
+    extras_require={
+        'test': ['pytest'],
+        'docs': ['sphinx', 'numpydoc']
+    }
+)
